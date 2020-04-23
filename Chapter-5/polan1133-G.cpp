6f7d6e42185317d2a9b105bad03b2159ac524c97
Yusuf Pangaribuan <yusuf.pangaribuan30@gmail.com>
2020-04-23 19:50:07 +0700

    first commit

---
+++ Chapter-5/polan1133-G.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x1, x2, y1, y2;
+	cin >> x1 >> y1 >> x2 >> y2;
+	x1 = x1 - x2;
+	y1 = y1 - y2;
+	
+	if(x1 < 0) x1 *= -1;
+	if(y1 < 0) y1 *= -1;
+	cout << x1 + y1 << endl;
+	return 0;
+}
