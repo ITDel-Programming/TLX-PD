1542498fd2a66b6225c5db1152981dfcf701939e
Yusuf Pangaribuan <yusufpangaribuan30@gmail.com>
2020-04-25 08:30:25 +0700

    tlx chapter 6 C

--- Chapter-6/polan113-B.cpp
+++ Chapter-6/polan113-B.cpp
@@ -3,16 +3,14 @@ using namespace std;
 
 int main() {
 	// your code goes here
-	int n;
-	int b;
-	int ans = 0;
+	int n, b, ans = 0;
 	cin >> n;
-	
+
 	for(int i = 0; i < n; i++) {
 		cin >> b;
 		ans += b;
 	}
-	
+
 	cout << ans << endl;
 	return 0;
 }
