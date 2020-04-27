3fec4b59350580c4c40d2e0bb8cc0f23fdf21c60
Friska Sianturi <friskasianturi23@gmail.com>
2020-04-27 22:07:45 +0700

    Update

--- Chapter-5/sirius19-G.cpp
+++ Chapter-5/sirius19-G.cpp
@@ -3,6 +3,14 @@ using namespace std;
 int main(){
     int x1,y1,x2,y2;
     cin>>x1>>y1>>x2>>y2;
-    cout<<abs(x1-x2) + abs(y1-y2)<<endl;
+    x1=x1-x2;
+    y1=y1-y2;
+    if(x1 < 0){
+        x1*=-1;
+    }
+    if(y1 < 0){
+        y1*=-1;
+    }
+    cout<<x1+y1<<endl;
     return 0;
 }
