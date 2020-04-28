268e38fba24b7210f65b3f2b31c0b3665293bf50
Alexander Nicholas Salim <alexandernsalim@gmail.com>
2020-04-28 17:09:09 +0700

    Add solution for problem A ch. 1

---
+++ Chapter-1/choxcoff-A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef unsigned long long ull;
+#define INF int(1e9)
+#define pb push_back
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cout << "Halo, dunia!\n";
+
+    return 0;
+}
